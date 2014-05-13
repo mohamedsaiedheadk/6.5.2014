7a89f5a081d39d8972fafaa1c48140311e31761f
mohamedheadk <mohamedheadk@hotmail.comgit config --global user.name mohamedheadkgit config --global user.email mohamedheadk@hotmail.com>
2014-05-13 14:38:14 +0200

    last commit

--- inheritance.php
+++ inheritance.php
@@ -1,22 +1,24 @@
 <?php
 
+
+
 interface Emp{ public function sal();}
-interface Driver {public function sal();}
-interface Student {public function Absence();}
+interface ForDriver {public function sal();}
+interface ForStudent {public function Absence();}
 
 
 
 abstract class Person {
 	
 	private $name ;
-	private $age ;
+	private $yeareOfBirth ;
 	private $gender ;
 	private $address ;
 	private $phone ;
 	
-	 public function __construct($name, $age ,$gender, $address, $phone) {
+	 public function __construct($name, $yearOfBirth ,$gender, $address, $phone) {
 		$this->name = $name;
-		$this->age = $age;
+		$this->yearOfBirth = $yearOfBirth;
 		$this->gender = $gender;
 		$this->phone = $phone;
 		$this->address = $address;
@@ -26,6 +28,10 @@ abstract class Person {
 	
 	abstract public function Hobby(); 
 	
+	//public function date(){return date ; }
+	
+	//public function getDate(){return "YOUR AGE IS : " . $this->date() -  $yearOfBirth ;}
+	
 	
 	
 	
@@ -33,36 +39,42 @@ abstract class Person {
 
 
 class Manger extends Person implements Emp{
+	
 	public function Hobby()
 	{echo "any thing";}
                         
-						public function sal(){echo"";}                     
-
-
-
+						public function sal(){echo" YOUR SAL IS :";}
+						                    
 }
-class Student extends Person implements Student{
+
+
+class Student extends Person implements ForStudent{
 	public function Hobby()
 	{echo "any thing";}
+	
 	public function Absence(){echo"";}
 	
 	}
-class Teacher extends Person implements Emp , Student{
+	
+	
+	
+class Teacher extends Person implements Emp , ForStudent{
 	public function Hobby()
 	{echo "any thing";}
 	public function sal(){echo"";}
 	public function Absence(){echo"";}
-	
+		
 	}
-class Driver extends person implements Driver{public function Hobby()
+class Driver extends person implements ForDriver{public function Hobby()
 {echo "any thing";}
 public function sal(){echo"";}
 }
 
-$Manger_ob = new Manger ('mohamed' ,30,'male','alex','01200099060');
+$Manger_ob = new Manger ('mohamed' ,1980,'male','alex','01200099060');
+//echo $Manger_ob ->getDate();
 
-$Student_ob = new student ('ahmed' ,31,'male','cairo','01090048638');
+$Student_ob = new student ('ahmed' ,1985,'male','cairo','01090048638');
 
-$Teacher_ob = new teacher ('rame' ,28,'male','tanta','01116461416');
+$Teacher_ob = new teacher ('rame' ,1990,'male','tanta','01116461416');
 
-$Driver_ob = new driver ('ali' ,22,'male','alex','035338831');
+$Driver_ob = new driver ('ali' ,2000,'male','alex','5400258');
